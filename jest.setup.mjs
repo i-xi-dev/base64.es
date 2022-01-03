56a16849e7cc001df881464df8a9122e7f2da2e4
i-xi-dev <naoyuki.isono@gmail.com>
2022-01-03 22:30:05 +0900

    1.1.3

--- jest.setup.mjs
+++ jest.setup.mjs
@@ -1,7 +1,4 @@
 // TODO 何故importが使えないのか
-// TODO 何故importが使えないのか
-
-// globalThisがNodeのと違うのでimportしないと使えないもの
 
 const { TransformStream } = require("node:stream/web");
 globalThis.TransformStream = TransformStream;
