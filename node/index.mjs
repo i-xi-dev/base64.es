1ec0fa10b91e11612e613520df74ad3ffb37c457
i-xi-dev <naoyuki.isono@gmail.com>
2022-04-30 17:21:15 +0900

    2.0.3

--- node/index.mjs
+++ node/index.mjs
@@ -1,6 +1,8 @@
 if ("process" in globalThis) {
   const mod = "node:stream/web";
-  globalThis.TransformStream = (await import(mod)).TransformStream;
+  if (!globalThis.TransformStream) {
+    globalThis.TransformStream = (await import(mod)).TransformStream;
+  }
 }
 
 export * from "../dist/index.js";
